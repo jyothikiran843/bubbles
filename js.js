e944074032004233efa68983e1ae0f27f02068f7
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-07-13 13:54:58 +0530

    removed background

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-var x,y;
 function scatter(){
     for(i in Array.from(document.getElementsByClassName('bubbles'))){
         document.getElementsByClassName('bubbles')[i].animate({transform:'translate('+Math.floor(Math.random() * (+600 - +1)) + +1+'px,'+Math.floor(Math.random() * (+300 - +1)) + +1+'px)'},6000)
